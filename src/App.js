46d5e00f3bd92752b3e70abbc779ed0a0acd994a
Saf-ire <carlosacostadominguez@gmail.com>
2021-06-18 16:49:26 -0600

    Fixing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import Main from "./pages/Main/Main"
 import NewBadge from "./pages/NewBadge/NewBadge"
 import LogIn from "./pages/Accounts/LogIn"
 import SignUp from "./pages/Accounts/SignUp"
-import Select from "./pages/Main/Selection"
 import error from "./pages/Error_404/Error"
 import Badges from "./pages/Badges/Badges"
 import BadgeDetails from './components/BadgeDetails'
 import EditBadge from "./pages/EditBadge/EditBadge"
-import NotFound from "./pages/Error_404/NotFound"
-
 
 function App() {
   return (
@@ -19,7 +16,6 @@ function App() {
       <Layout>
         <Switch>
           <Route exact path="/" component={Main}></Route>
-          <Route exact path="/select" component={Select}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
           <Route exact path="/log_in" component={LogIn}></Route>
           <Route exact path="/sign_up" component={SignUp}></Route>
